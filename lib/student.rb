915b176d18e41038390395a6ac7a2bcf0628a1ac
Jesse Wenger <wengerj@gmail.com>
2021-03-25 19:25:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute(students.return_)
+    @id = DB[:conn].execute(students.last_)
   end
 end
 
