45def6273bc35083b830062cbb3f92662af6cdf9
Jesse Wenger <wengerj@gmail.com>
2021-03-25 19:56:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
-      LIMIT 
+      LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
